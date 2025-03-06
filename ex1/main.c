2aedc4b5c32c9cf2b2b8cd044668755eca0ea2c5
liu930324 <113400161+liu930324@users.noreply.github.com>
2025-03-06 15:51:29 +0800

    Update main.c

--- ex1/main.c
+++ ex1/main.c
@@ -1,46 +1,76 @@
-int main(){
-    int a[]={0,1,2};
-    char b[]="ABC";
-    float c[] = {1.1, 1.2, 1.3 };
-    FILE*fp;
-    if((fp=fopen("a.bin","wb+"))==NULL){
-        printf("Cannot open the file...");
-        exit(1);
-    }
-    if(fwrite(a,sizeof(a),1,fp)!=1){
-        printf("File write erro...\n");
-    }
-    fseek(fp,0,SEEK_SET);
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a[] = {0, 1, 2};
+    char b[] = "ABC";
+    float c[] = {1.1, 1.2, 1.3};
     
-    if(fread(a,sizeof(a),1,fp)!=1){
-        printf("File read erro...\n");
-    }
-    //-----------------------------------
-    if((fp=fopen("a.bin","wb+"))==NULL){
-        printf("Cannot open the file...");
+    FILE* fp;
+
+    fp = fopen("a.bin", "wb+");
+    if (fp == NULL) {
+        printf("無法打開文件...\n");
         exit(1);
-    }
-    if(fwrite(b,sizeof(b),1,fp)!=1){
-        printf("File write erro...\n");
-    }
-    fseek(fp,0,SEEK_SET);
-    
-    if(fread(b,sizeof(b),1,fp)!=1){
-        printf("File read erro...\n");
-    }
-    //-----------------------------------
-    if((fp=fopen("a.bin","wb+"))==NULL){
-        printf("Cannot open the file...");
-        exit(1);
-    }
-    if(fwrite(c,sizeof(c),1,fp)!=1){
-        printf("File write erro...\n");
-    }
-    fseek(fp,0,SEEK_SET);
-    
-    if(fread(c,sizeof(c),1,fp)!=1){
-        printf("File read erro...\n");
     }
 
+    if (fwrite(a, sizeof(int), sizeof(a) / sizeof(int), fp) != sizeof(a) / sizeof(int)) {
+        printf("寫入整數陣列時出錯...\n");
+        fclose(fp);
+        exit(1);
+    }
+
+    fseek(fp, 0, SEEK_SET);
+    int read_a[3];
+    if (fread(read_a, sizeof(int), sizeof(a) / sizeof(int), fp) != sizeof(a) / sizeof(int)) {
+        printf("讀取整數陣列時出錯...\n");
+        fclose(fp);
+        exit(1);
+    } else {
+        for (int i = 0; i < 3; i++) {
+            printf("%d ", read_a[i]);
+        }
+        printf("\n");
+    }
+
+    fseek(fp, 0, SEEK_END);
+    if (fwrite(b, sizeof(char), sizeof(b), fp) != sizeof(b)) {
+        printf("寫入字符串陣列時出錯...\n");
+        fclose(fp);
+        exit(1);
+    }
+
+    fseek(fp, sizeof(a), SEEK_SET);
+    char read_b[4];
+    if (fread(read_b, sizeof(char), sizeof(b), fp) != sizeof(b)) {
+        printf("讀取字符串陣列時出錯...\n");
+        fclose(fp);
+        exit(1);
+    } else {
+        printf("%s\n", read_b);
+    }
+
+    fseek(fp, 0, SEEK_END);
+    if (fwrite(c, sizeof(float), sizeof(c) / sizeof(float), fp) != sizeof(c) / sizeof(float)) {
+        printf("寫入浮點數陣列時出錯...\n");
+        fclose(fp);
+        exit(1);
+    }
+
+    fseek(fp, sizeof(a) + sizeof(b), SEEK_SET);
+    float read_c[3];
+    if (fread(read_c, sizeof(float), sizeof(c) / sizeof(float), fp) != sizeof(c) / sizeof(float)) {
+        printf("讀取浮點數陣列時出錯...\n");
+        fclose(fp);
+        exit(1);
+    } else {
+        for (int i = 0; i < 3; i++) {
+            printf("%2f ", read_c[i]);
+        }
+        printf("\n");
+    }
+
+    fclose(fp);
     return 0;
 }
+
