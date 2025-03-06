3584f2cd60fffa871f774a28ef4ace4ecfbc56cf
liu930324 <113400161+liu930324@users.noreply.github.com>
2025-03-06 16:43:42 +0800

    Update main5.c

--- hw1/main5.c
+++ hw1/main5.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-#include <string.h>
 
-void print_sp(int i,int n){
-    for(int m=0;m<=(i-n)-1;m++){
+void print_sp(int i, int n) {
+    for (int m = 0; m < (n - i); m++) {
         printf(" ");
     }
 }
-void print_num(int n){
-    for(int i=1;i<=n;i++){
-        printf("%d ",n);
+
+void print_num(int n) {
+    for (int i = 1; i <= n; i++) {
+        printf("%d", n);
+        if (i < n) printf(" ");
     }
     printf("\n");
 }
-int main()
-{
-    char input[30];
-    char output[60];
-    scanf("%d",input);
-    for(int i=1;i<=rows;i++){
-        print_sp(rows,i);
+
+int main() {
+    int rows;
+    scanf("%d", &rows);
+    for (int i = 1; i <= rows; i++) {
+        print_sp(i, rows);
         print_num(i);
     }
+    return 0;
 }
