da0a7da3baa33057c3d0f19e487cf93230cb6939
liu930324 <113400161+liu930324@users.noreply.github.com>
2025-05-08 14:57:51 +0800

    Update main.cpp

--- ex5/main.cpp
+++ ex5/main.cpp
@@ -2,63 +2,55 @@
 #include <string>
 using namespace std;
 
-// 基礎汽車類別
 class Car {
 public:
     string m_brand;
     string m_model;
     int m_year;
     int m_MaxSeating;
-    
-    // 建構函式
-    Car(string x, string y, int z, int s) {
-        m_brand = x;
-        m_model = y;
-        m_year = z;
-        m_MaxSeating = s;
+    string m_DriveMode;
+
+    Car(string x, string y, int z, int s, string t)
+        : m_brand(x), m_model(y), m_year(z), m_MaxSeating(s), m_DriveMode(t) {
+        info_print();
     }
-    
-    // 獲取座位數的方法
+
     int get_m_MaxSeating() {
         return m_MaxSeating;
     }
+
+    string get_m_DriveMode() {
+        return m_DriveMode;
+    }
+    
+    void info_print() {
+        cout << "Constructing " << m_brand << "_Car" << endl;
+        cout << m_brand << " : Drive Mode = " << m_DriveMode << endl;
+    }
 };
 
-// BMW汽車類別
 class BMW_Car : public Car {
 public:
-    BMW_Car(string model, int year, int maxSeating) 
-        : Car("BMW", model, year, maxSeating) {
-        cout << "Constructing BMW_Car" << endl;
-        cout << "BMW : Drive Mode = Rear-wheel" << endl;
-    }
+    BMW_Car(string model, int year, int maxSeating, string driveMode)
+        : Car("BMW", model, year, maxSeating, driveMode) {}
 };
 
-// Audi汽車類別
 class AUDI_Car : public Car {
 public:
-    AUDI_Car(string model, int year, int maxSeating) 
-        : Car("Audi", model, year, maxSeating) {
-        cout << "Constructing AUDI_Car" << endl;
-        cout << "Audi : Drive Mode = Front-wheel" << endl;
-    }
+    AUDI_Car(string model, int year, int maxSeating, string driveMode)
+        : Car("Audi", model, year, maxSeating, driveMode) {}
 };
 
-// Benz汽車類別
 class BENZ_Car : public Car {
 public:
-    BENZ_Car(string model, int year, int maxSeating) 
-        : Car("Benz", model, year, maxSeating) {
-        cout << "Constructing BENZ_Car" << endl;
-        cout << "Benz : Drive Mode = Front-wheel" << endl;
-    }
+    BENZ_Car(string model, int year, int maxSeating, string driveMode)
+        : Car("Benz", model, year, maxSeating, driveMode) {}
 };
 
 int main() {
-    // 建立不同品牌的汽車物件，建構函式會直接輸出所需格式的信息
-    BMW_Car car_1("X5", 2023, 6);
-    AUDI_Car car_2("A1", 2021, 2);
-    BENZ_Car car_3("S-Class", 2022, 5);
-    
+    BMW_Car car_1("X5", 2023, 6, "Rear-wheel");
+    AUDI_Car car_2("A1", 2021, 2, "Front-wheel");
+    BENZ_Car car_3("S-Class", 2022, 5, "Front-wheel");
+
     return 0;
 }
